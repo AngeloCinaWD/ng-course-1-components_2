0adf720b7e9a929498988dd08ef3c315732ab149
unknown <a.cina@procreatech.com>
2024-09-18 18:19:30 +0200

    35. Angular Content Child Decorator - In-Depth Explanation

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,5 +1,13 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
+import {
+  Component,
+  ContentChild,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+} from "@angular/core";
 import { Course } from "../model/course";
+import { CourseImageComponent } from "../course-image/course-image.component";
 
 @Component({
   selector: "course-card",
@@ -22,9 +30,21 @@ export class CourseCardComponent {
   @Output()
   courseSelected = new EventEmitter<Course>();
 
+  // CONTENTCHILD DECORATOR mi permette di ottenere un riferimento diretto ad alcuni dei contenuti proiettati
+  // se mi riferisco ad un elemento html normale nel content tag ottengo un ElementRef, quindi un elemento nativo del dom
+  // posso comunque riferirmi anche ad un componente che è nel content tag, ad esempio proietto l'immagine tramite componente CourseImageComponent
+  @ContentChild(CourseImageComponent)
+  immagineComponente: CourseImageComponent;
+
+  // altrimenti per ottenere l'elemento nativo del dom aggiungo la proprietà read elementref
+  @ContentChild(CourseImageComponent, { read: ElementRef })
+  immagineComponenteElementRef: CourseImageComponent;
+
   onCourseViewed() {
     // quando clicco emetterò l'evento custom courseSelected e gli passo quello che voglio trasmettere come payload
     this.courseSelected.emit(this.course);
+    console.log(this.immagineComponente);
+    console.log(this.immagineComponenteElementRef);
   }
 
   checkIconUrl() {
