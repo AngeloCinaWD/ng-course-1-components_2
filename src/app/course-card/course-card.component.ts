ecfcc0bdb74cfa37f34f58ec2e9a4d8e005c0777
unknown <a.cina@procreatech.com>
2024-09-18 20:14:05 +0200

    36. Angular Content Children and the AfterContentInit Lyfecycle Hook

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,10 +1,13 @@
 import {
   Component,
   ContentChild,
+  ContentChildren,
+  contentChildren,
   ElementRef,
   EventEmitter,
   Input,
   Output,
+  QueryList,
 } from "@angular/core";
 import { Course } from "../model/course";
 import { CourseImageComponent } from "../course-image/course-image.component";
@@ -33,12 +36,30 @@ export class CourseCardComponent {
   // CONTENTCHILD DECORATOR mi permette di ottenere un riferimento diretto ad alcuni dei contenuti proiettati
   // se mi riferisco ad un elemento html normale nel content tag ottengo un ElementRef, quindi un elemento nativo del dom
   // posso comunque riferirmi anche ad un componente che è nel content tag, ad esempio proietto l'immagine tramite componente CourseImageComponent
+  // per essere sicuri che le variabili siano riempite si utilizza il LifeCycle Hook AfterContentInit
   @ContentChild(CourseImageComponent)
   immagineComponente: CourseImageComponent;
 
   // altrimenti per ottenere l'elemento nativo del dom aggiungo la proprietà read elementref
+  // in caso di più elementi dello stesso tipo punta al primo
   @ContentChild(CourseImageComponent, { read: ElementRef })
   immagineComponenteElementRef: CourseImageComponent;
+
+  // CONTENTCHILDREN ci permmette di creare riferimenti a collection
+  //  restituisce una querylist
+  @ContentChildren(CourseImageComponent)
+  images: QueryList<CourseImageComponent>;
+  // con la proprietà read element ref posso riferirmi ad elementi nativi del dom
+  @ContentChildren(CourseImageComponent, { read: ElementRef })
+  imagesElementRef: QueryList<CourseImageComponent>;
+
+  ngAfterContentInit(): void {
+    //Called after ngOnInit when the component's or directive's content has been initialized.
+    //Add 'implements AfterContentInit' to the class.
+    console.log(this.immagineComponente);
+    console.log(this.images.toArray());
+    console.log(this.imagesElementRef.toArray());
+  }
 
   onCourseViewed() {
     // quando clicco emetterò l'evento custom courseSelected e gli passo quello che voglio trasmettere come payload
